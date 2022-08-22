d5f3346a48a66392d9c3ab6376debfda26bf40d3
Eduardo Galvão <edu.tads@outlook.com>
2022-08-22 20:13:42 -0300

    Create save transaction, update balance and create credit card methods

--- codebank/domain/transaction.go
+++ codebank/domain/transaction.go
@@ -6,7 +6,7 @@ import (
 )
 
 type TransactionRepository interface {
-	SaveTransaction(transaction Transaction, creditCard CreditCard) error
+	error
 	GetCreditCard(creditCard CreditCard) (CreditCard, error) // One function can return more than one value
 	CreateCreditCard(creditCard CreditCard) error
 }
--- codebank/infrastructure/repository/transaction.go
+++ codebank/infrastructure/repository/transaction.go
@@ -1,6 +1,9 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/srpulga/CodeBank/domain"
+)
 
 type TransactionRepositoryDB struct {
 	db *sql.DB // Package for GO to handle database
@@ -8,4 +11,71 @@ type TransactionRepositoryDB struct {
 
 func NewTransactionRepositoryDB(db *sql.DB) *TransactionRepositoryDB {
 	return &TransactionRepositoryDB{db: db}
+}
+
+func (t *TransactionRepositoryDB) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
+	//Prepared Statements
+	stmt, err := t.db.Prepare(`INSERT INTO transactions (id, credit_card_id, amount, status, description, store, created_ad) 
+									 VALUES ($1, $2, $3, $4, $5, $6, $7)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(
+		transaction.ID,
+		creditCard.ID,
+		transaction.Amount,
+		transaction.Status,
+		transaction.Description,
+		transaction.Store,
+		transaction.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	if transaction.Status == "approved" {
+		err = t.updateBalance(creditCard)
+		if err != nil {
+			return err
+		}
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TransactionRepositoryDB) updateBalance(creditCard domain.CreditCard) error {
+	_, err := t.db.Exec("UPDATE credit_cards SET balance = $1 WHERE id = $2",
+		creditCard.Balance, creditCard.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TransactionRepositoryDB) CreateCreditCard(creditCard domain.CreditCard) error {
+	stmt, err := t.db.Prepare(`INSERT INTO credit_cards(id, name, number, expiration_month, expiration_year, CVV, balance, balance_limit) 
+								VALUES($1,$2,$3,$4,$5,$6,$7,$8)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(
+		creditCard.ID,
+		creditCard.Name,
+		creditCard.Number,
+		creditCard.ExpirationMonth,
+		creditCard.ExpirationYear,
+		creditCard.CVV,
+		creditCard.Balance,
+		creditCard.Limit,
+	)
+	if err != nil {
+		return err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
