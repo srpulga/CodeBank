c00f6a73dde819f74f284e14b231a2f56cf730d0
Eduardo Galvão <edu.tads@outlook.com>
2022-08-22 20:24:19 -0300

    Create Process Transaction useCase

---
+++ codebank/usecase/process_transaction.go
@@ -0,0 +1,18 @@
+package usecase
+
+import (
+	"github.com/srpulga/CodeBank/domain"
+	"github.com/srpulga/CodeBank/dto"
+)
+
+type UseCaseTransaction struct {
+	TransactionRepository domain.TransactionRepository
+}
+
+func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
+	return UseCaseTransaction{TransactionRepository: transactionRepository}
+}
+
+func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
+
+}
